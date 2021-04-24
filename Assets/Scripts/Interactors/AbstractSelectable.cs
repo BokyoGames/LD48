e40f30d863d56eb1f076655ec32e7b3da2ccb576
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-24 14:02:57 +0900

    Added camera preview selection placeholder

--- Assets/Scripts/Interactors/AbstractSelectable.cs
+++ Assets/Scripts/Interactors/AbstractSelectable.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public abstract class AbstractSelectable : MonoBehaviour
 {
+
+    // This value indicates at which depth the object is in the layer hierarchy.
+    // This way the AI can decide if they need to use a ladder to go up/down
+    // a layer to reach the same depth as the interactable they are trying to
+    // reach or not.
+    public int Depth = 0;
+
     public virtual void TriggerSelection() {
         var selectable = GetComponentInChildren<UISelectableLogic>();
         selectable.ToggleSelectable(true);
