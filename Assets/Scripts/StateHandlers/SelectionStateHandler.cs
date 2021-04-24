5c55b6da3963b9584a9e69457b9e4fc5cfec74af
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-24 14:29:19 +0900

    Now camera preview chooses the right target preview

--- Assets/Scripts/StateHandlers/SelectionStateHandler.cs
+++ Assets/Scripts/StateHandlers/SelectionStateHandler.cs
@@ -10,6 +10,10 @@ public class SelectionStateHandler : MonoBehaviour
     // Object that is interactable
     public Interactable CurrentInteractableSelection;
 
+    // TODO: maybe refactor this
+    public Camera PreviewTargetCamera;
+    public GameObject PreviewTargetCameraGroup;
+
     public void OnClicked(Interactor interactor) {
         if(CurrentInteractorSelection != null) {
             CurrentInteractorSelection.TriggerUnselection();
@@ -22,17 +26,20 @@ public class SelectionStateHandler : MonoBehaviour
                     CurrentInteractableSelection.TriggerUnselection();
                     CurrentInteractableSelection = null;
                 }
+                previewCameraTargetCheck();
                 return;
             }
         }
         // Start new selection
         interactor.TriggerSelection();
         CurrentInteractorSelection = interactor;
+        previewCameraTargetCheck();
     }
 
     public void OnClicked(Interactable interactable) {
         if(CurrentInteractorSelection == null) {
             // We can't select an interactable if the interactor is not active
+            previewCameraTargetCheck();
             return;
         }
         if(CurrentInteractableSelection != null) {
@@ -40,11 +47,33 @@ public class SelectionStateHandler : MonoBehaviour
             if(CurrentInteractableSelection == interactable) {
                 // We clicked on the same interactable twice, remove it and leave
                 CurrentInteractableSelection = null;
+                previewCameraTargetCheck();
                 return;
             }
         }
         interactable.TriggerSelection();
         CurrentInteractableSelection = interactable;
+        previewCameraTargetCheck();
     }
 
+    // In case the target/interactor has changed, we double check that the 
+    // preview camera is being rendered correctly (or we hide it)
+    private void previewCameraTargetCheck() {
+        if(CurrentInteractorSelection != null) {
+            // Our selected interactor is not targeting anything, hide the camera preview
+            if(CurrentInteractorSelection.InteractionTarget == null) {
+                PreviewTargetCameraGroup.SetActive(false);
+                return;
+            }
+            // Make sure the camera is pointing to our interaction target
+            Vector3 newPosition = CurrentInteractorSelection.InteractionTarget.transform.position;
+            newPosition.z = PreviewTargetCamera.transform.position.z;
+            PreviewTargetCamera.transform.position = newPosition;
+            // Show the camera preview
+            PreviewTargetCameraGroup.SetActive(true);
+        } else {
+            // No interactor selected, hide the camera preview.
+            PreviewTargetCameraGroup.SetActive(false);
+        }
+    }
 }
