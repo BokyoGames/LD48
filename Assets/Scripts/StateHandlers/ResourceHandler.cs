89de15fbecd5106155e3077e23c9aa6ded79607e
Danger <danger.recheng@hotmail.it>
2021-04-24 12:59:16 +0200

    feat(script): add resource handler

---
+++ Assets/Scripts/StateHandlers/ResourceHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int stone = 10;
+    public int iron = 0;
+    public int gold = 0;
+    public int diamond = 0;
+    public int mithril = 0;
+    public int happiness = 0;
+
+    public int max_stone = 10;
+    public int max_iron = 10;
+    public int max_gold = 10;
+    public int max_diamond = 10;
+    public int max_mithril = 10;
+    public int max_happiness = 100;
+
+    void Start()
+    {
+        
+    }
+
+    public int getResourceType(string type)
+    {
+        switch(type)
+        {
+            case "stone":
+                return stone;
+            case "iron":
+                return iron;  
+            case "gold":
+                return gold;              
+            case "diamond":
+                return diamond;              
+            case "mithril":
+                return mithril;              
+            case "happiness":
+                return happiness;
+            default:
+                return -1;  
+        }
+    }
+
+    public int getResourceMaxType(string type)
+    {
+        switch(type)
+        {
+            case "stone":
+                return max_stone;
+            case "iron":
+                return max_iron;  
+            case "gold":
+                return max_gold;              
+            case "diamond":
+                return max_diamond;              
+            case "mithril":
+                return max_mithril;              
+            case "happiness":
+                return max_happiness;
+            default:
+                return -1;  
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StateHandlers/ResourceUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceUpdater : MonoBehaviour
+{
+    ResourceHandler resources;
+    string resource_type;
+    private Text text_field;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resources = GameObject.Find("ResourceContainer").GetComponent<ResourceHandler>();
+        resource_type = name.ToLower();
+        text_field = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text_field.text = resources.getResourceType(resource_type).ToString() + " / " + resources.getResourceMaxType(resource_type).ToString();
+    }
+}
