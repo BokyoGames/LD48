9e835858b592219ca1c3b023ae73f2635aa92467
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-26 08:09:46 +0900

    Added template for music transitions WIP

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -6,7 +6,17 @@ public class MusicManager : MonoBehaviour
 {
     public AudioSource Music;
 
+    public List<AudioSource> MusicDepth;
+
     void Start() {
         Music.playOnAwake = true;
+    }
+
+    void Update()  {
+        if(!Music.isPlaying)
+            return;
+        //for(int i = 0; i < MusicDepth.Count; i++) {
+
+        //}
     }
 }
