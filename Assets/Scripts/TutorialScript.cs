4402e8d0a19e9404a9e92162baa3ee351d20d2da
Danger <danger.recheng@hotmail.it>
2021-04-25 23:56:58 +0200

    feat(tuorial): fix tutorial

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -5,28 +5,37 @@ using UnityEngine.UI;
 
 public class TutorialScript : MonoBehaviour
 {
-    public int DisplayTime = 5000;
+    public float DisplayTime = 10f;
     public Text text_field;
 
     // Start is called before the first frame update
     public string[] tutorials = new string[] {
-        "Click on the dwarf and on a target and tell him to go dig", 
-        "You can build houses so more dwarves can move into your city\nClick on the dwarf and on a building target and tell him what to build.", 
-        "Some special houses let you spawn new units. Try building one and assigning one of your dwarves to operate it.",
-        "Be careful! You never know what you’ll find the deeper you dig.\nTo attact an enemy select a dwarf and an enemy and click use to attack.",
-        "If you are injured, try building a hospital.",
-        "Some units can specialize in building or fighting, allocate your resources carefully.",
-        "Never dig straight down."
+        "Dwarves can dig resources, which are needed for buildings. Sometimes they will also open a passage to a new layer while digging.", 
+        "Houses are where new Dwarves grow. Send a Dwarf to a House to increase your population.", 
+        "Deposits increase storage capacity. Taverns increase the amount of Dwarves that can live in your city.",
+        "These mines are filled with hostile Goblins, slay them before you proceed!",
+        "Sending Dwarves to the Masonry will let you recruit specialized Builders.",
+        "Dwarves can go to the Barracks to train specialized Soldiers.",
+        "",
+        "Trolls in the cavern, be careful!",
+        "Injured Dwarves can be sent to a Hospital to heal.",
+        "",
+        "Giant Spiders?! Prepare for an epic battle!",
+        "",
+        "You found Mithril! Defeat the monsters and dig as much as you can!"
     };
 
     public void DisplayTutorial()
     {
         if(DataHandler.Handler.DepthReached < tutorials.Length)
         {
-            gameObject.SetActive(true);
-            text_field.text =  tutorials[DataHandler.Handler.DepthReached];
+            if(tutorials[DataHandler.Handler.DepthReached] != "")
+            {
+                gameObject.SetActive(true);
+                text_field.text =  tutorials[DataHandler.Handler.DepthReached];
 
-            Invoke("HideTutorial", 10f);
+                Invoke("HideTutorial", DisplayTime);
+            }
         }
     }
 
