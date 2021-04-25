ae2006d84fc5885461eb548d28843ffff9171f45
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-26 07:55:19 +0900

    Added new selectable sprites

--- Assets/Scripts/UISelectableLogic.cs
+++ Assets/Scripts/UISelectableLogic.cs
@@ -12,14 +12,18 @@ public class UISelectableLogic : MonoBehaviour
     public float XAnimationIntensity;
     [Range(0,1)]
     public float YAnimationIntensity;
+    [Range(0,1)]
+    public float YAnimationMovementIntensity = 0.5f;
     [Range(0, 2000)]
     public int AnimationSpeed;
 
     private Vector3 originalScale;
+    private Vector3 originalPosition;
 
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
         originalScale = transform.localScale;
+        originalPosition = transform.localPosition;
         startTime = Time.time;
     }
 
@@ -30,9 +34,13 @@ public class UISelectableLogic : MonoBehaviour
             Vector2 modifier = new Vector2(value * (XAnimationIntensity/2f), value * (YAnimationIntensity/2));
             Vector2 temp = (Vector2)originalScale + modifier;
             transform.localScale = new Vector3(temp.x, temp.y, 1);
+            modifier = new Vector2(0, value * (YAnimationMovementIntensity/2));
+            temp = (Vector2)originalPosition + modifier;
+            transform.localPosition = temp;
         } else {
             // Make sure the original scale is reset so we don't get funny bugs
             transform.localScale = originalScale;
+            transform.localPosition = originalPosition;
         }
     }
 
