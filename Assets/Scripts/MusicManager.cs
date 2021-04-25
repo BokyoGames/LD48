988fd4f0e8e3bcf82c080a1ca6b1af4a0558c4ea
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-25 10:28:08 +0900

    Added basic music logic

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource Music;
+
+    void Start() {
+        Music.playOnAwake = true;
+    }
+}
