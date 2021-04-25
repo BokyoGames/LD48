fd0b1ebdd5ad8fb2e31c3c1612caff4decaacc5e
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-26 08:40:12 +0900

    Fixed layered audio

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -8,6 +8,9 @@ public class MusicManager : MonoBehaviour
 
     public List<AudioSource> MusicDepth;
 
+    [Range(0,1)]
+    public float MusicModifier;
+
     void Start() {
         Music.playOnAwake = true;
     }
@@ -15,8 +18,23 @@ public class MusicManager : MonoBehaviour
     void Update()  {
         if(!Music.isPlaying)
             return;
-        //for(int i = 0; i < MusicDepth.Count; i++) {
 
-        //}
+        float maxVolume = Music.volume * MusicModifier;
+        int depth = DataHandler.Handler.DepthReached;
+        float layer1Step = 0.083f;
+        float layer2Step = 0.125f;
+        float layer3Step = 0.25f;
+        if(depth == 0)
+            return;
+        if(depth <= 4) {
+            MusicDepth[0].volume = maxVolume * layer1Step * depth;
+        } else if(depth <= 8) {
+            MusicDepth[0].volume = maxVolume * layer1Step * depth;
+            MusicDepth[1].volume = maxVolume * layer2Step * (depth - 4);
+        } else {
+            MusicDepth[0].volume = maxVolume * layer1Step * depth;
+            MusicDepth[1].volume = maxVolume * layer2Step * (depth - 4);
+            MusicDepth[2].volume = maxVolume * layer3Step * (depth - 8);
+        }
     }
 }
