8e3ebb63af872060cf0ab76e5ee3d1a573e92df2
Danger <danger.recheng@hotmail.it>
2021-04-26 20:02:27 +0200

    feat(end_balrog): add fading in text

---
+++ Assets/FadeInText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInText : MonoBehaviour
+{
+
+    public float wait_time = 10f;
+    public float fade_in_time = 10f;
+
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        text.CrossFadeAlpha(0f, 0f, false);
+        Invoke("Fade", wait_time);
+    }
+
+    void Fade()
+    {
+        text.CrossFadeAlpha(1f, fade_in_time, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
