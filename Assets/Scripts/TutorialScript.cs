496f5814fed1f18759f80d39076201731dfb25a0
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-26 20:48:48 +0900

    Fixed tutorial

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -9,29 +9,12 @@ public class TutorialScript : MonoBehaviour
     public Text text_field;
 
     // Start is called before the first frame update
-    public string[] tutorials = new string[] {
-        "Dwarves can dig resources, which are needed for buildings. Sometimes they will also open a passage to a new layer while digging.", 
-        "Houses are where new Dwarves grow. Send a Dwarf to a House to increase your population.", 
-        "Deposits increase storage capacity. Taverns increase the amount of Dwarves that can live in your city.",
-        "These mines are filled with hostile Goblins, slay them before you proceed!",
-        "Sending Dwarves to the Masonry will let you recruit specialized Builders.",
-        "Dwarves can go to the Barracks to train specialized Soldiers.",
-        "",
-        "Trolls in the cavern, be careful!",
-        "Injured Dwarves can be sent to a Hospital to heal.",
-        "",
-        "Giant Spiders?! Prepare for an epic battle!",
-        "",
-        "You found Mithril! Defeat the monsters and dig as much as you can!"
-    };
-    // void Start() {
-    //     Debug.Log("Start");
-    //     DisplayTutorial();
-    // }
-    // void Awake() {
-    //     Debug.Log("Awake");
-    //     DisplayTutorial();
-    // }
+    public string[] tutorials;
+
+    void Start() {
+        Debug.Log("Start");
+        DisplayTutorial();
+    }
 
     public void DisplayTutorial() {
         if(DataHandler.Handler.DepthReached < tutorials.Length) {
