6eb03c0e1f0f76f9a9de65d7947b3bc3c1498814
Federico 'Morg' Pareschi <morgawr@gmail.com>
2021-04-27 08:42:47 +0900

    Added mouse smoothing

--- Assets/Scripts/MouseDraggableCamera.cs
+++ Assets/Scripts/MouseDraggableCamera.cs
@@ -9,13 +9,16 @@ public class MouseDraggableCamera : MonoBehaviour
 
     public float LimitPerDepth;
     public float MaxYValue;
+    [Range(0, 1f)]
+    public float ClampValue;
 
     void LateUpdate() {
         if (Input.GetMouseButton(1)) {
 
             var newPosition = transform.position;
 
-            newPosition.y -= Input.GetAxis("Mouse Y") * DragSpeed * Time.deltaTime;
+
+            newPosition.y -= Mathf.Clamp(Input.GetAxis("Mouse Y"), -ClampValue, ClampValue) * DragSpeed * Time.deltaTime;
 
             newPosition.y = Mathf.Clamp(newPosition.y, DataHandler.Handler.DepthReached * LimitPerDepth, MaxYValue);
 
